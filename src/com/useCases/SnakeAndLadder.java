2038f0a07c32098a35740e00b4399fa456e82965
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-10 00:28:59 +0530

    [kshitija] : player rolled the dice

--- src/com/useCases/SnakeAndLadder.java
+++ src/com/useCases/SnakeAndLadder.java
@@ -1,10 +1,14 @@
 package com.useCases;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int player1=0;
-
+		Random r=new Random();
+        int diceRoll=r.nextInt(7);
+        System.out.println(diceRoll);
 	}
 
 }
