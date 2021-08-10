21e9cbd9d0dd773957fbc7b2aac00b53ed8e7e7b
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-10 13:32:36 +0530

    [kshitija]: repeating till reaches winning position

--- src/com/useCases/SnakeAndLadder.java
+++ src/com/useCases/SnakeAndLadder.java
@@ -1,30 +1,54 @@
 package com.useCases;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int player1=0;
+		//System.out.println(player1);
 		Random r=new Random();
-        int diceRoll=r.nextInt(7);
-        System.out.println(diceRoll);
-	
-        player1+=diceRoll;
-        int snake=r.nextInt(50);
-        int ladder=r.nextInt(50);
-        
+		
+       
+        while(player1!=100&&player1<100) { 
+        	
+        	 int diceRoll=r.nextInt(7);
+             //System.out.println(diceRoll);
+        	 if(diceRoll==0)           
+             {
+                diceRoll++;
+             }
+        	 
+        	
+           player1=player1+diceRoll;
+           System.out.println("player1   " +player1);
+             if(player1<0) {
+        		 
+              	player1=0;	
+              	
+              	}else if(player1==100) {
+              		break;
+              	}
+             
+        int snake=r.nextInt(100);
+        int ladder=r.nextInt(100);
+
         System.out.println(snake);
         System.out.println(ladder);
-        if(player1==snake) {
+        
+       if(player1==snake) {
         	player1-=diceRoll;
-        	
+
         }else if(player1==ladder) {
         	player1+=diceRoll;
         }
-        
-        System.out.println(player1);
+       System.out.println(player1);
 	}
+
+       
+	}
+	
+
 }
+	
+
