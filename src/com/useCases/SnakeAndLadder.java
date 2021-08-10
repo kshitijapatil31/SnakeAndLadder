f0bcb5f90dde02d428d4306b6f4dae45368b0140
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-10 14:51:51 +0530

    [kshitija]: exact winning position

--- src/com/useCases/SnakeAndLadder.java
+++ src/com/useCases/SnakeAndLadder.java
@@ -5,50 +5,55 @@ import java.util.Random;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		int player1=0;
-		//System.out.println(player1);
-		Random r=new Random();
-		
-       
-        while(player1!=100&&player1<100) { 
-        	
-        	 int diceRoll=r.nextInt(7);
-             //System.out.println(diceRoll);
-        	 if(diceRoll==0)           
-             {
-                diceRoll++;
-             }
-        	 
-        	
-           player1=player1+diceRoll;
-           System.out.println("player1   " +player1);
-             if(player1<0) {
-        		 
-              	player1=0;	
-              	
-              	}else if(player1==100) {
-              		break;
-              	}
-             
-        int snake=r.nextInt(100);
-        int ladder=r.nextInt(100);
+		int player1 = 0, player2 = 0, flag = 0;
+		// System.out.println(player1);
+		int turn1 = 1, turn2 = 2;
+		Random r = new Random();
 
-        System.out.println(snake);
-        System.out.println(ladder);
-        
-       if(player1==snake) {
-        	player1-=diceRoll;
+		while (player1 != 100 && player1 < 100 || player2 != 100 && player2 < 100) {
+			if (flag == 0) {
+				turn1++;
 
-        }else if(player1==ladder) {
-        	player1+=diceRoll;
-        }
-       System.out.println(player1);
+				int diceRoll = r.nextInt(7);
+				System.out.println("dice"+diceRoll);
+				if (diceRoll == 0) {
+					diceRoll++;
+				}
+
+				System.out.println("player1   " + player1);
+				if ((player1 += diceRoll) < 0) {
+
+					player1 = 0;
+
+				} else if ((player1 += diceRoll) == 100) {
+					
+					break;
+					
+				} else if ((player1 += diceRoll) > 100) {
+					System.out.println("You will have to wait for the next turn, you have exceeded 100.");
+					flag = 1;
+					
+				} else {
+					
+					////player1 += diceRoll;
+					System.out.println("position "+(player1 += diceRoll));
+					int snake = r.nextInt(100);
+					int ladder = r.nextInt(100);
+
+					System.out.println("snake"+snake);
+					System.out.println("ladder"+ladder);
+
+					if ((player1 += diceRoll) == snake) {
+						player1 -= diceRoll;
+
+					} else if ((player1 += diceRoll) == ladder) {
+						player1 += diceRoll;
+					}
+					
+					System.out.println("abc"+player1);
+				}
+			}
+
+		}
 	}
-
-       
-	}
-	
-
 }
-	
-
